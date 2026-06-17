0f98f32b5df2d38d5e7d80a95dfc508b7da27554
Israel <israeljovemtech@gmail.com>
2026-06-17 20:17:27 -0300

    Feat: Adição do ENV e remoção das chaves do supabase expostas no código

--- src/servicos/supabase.js
+++ src/servicos/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://pyzmekxxfufkdivbjung.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5em1la3h4ZnVma2RpdmJqdW5nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE3MjY1MzksImV4cCI6MjA5NzMwMjUzOX0.DdoRgNiV3Km642bazf9X1lq2_JeYuLz_uB_gD9WxGno'
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient(
   supabaseUrl,
